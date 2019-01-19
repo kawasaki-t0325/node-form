6f57f6f32731d30321166fe61a5e553ce885db33
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-01-19 15:30:02 +0900

    formの値を取得できるようにする

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/view');
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.render('./index');
-})
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.end();
+});
 
 app.listen(3000);
