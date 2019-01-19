c293932a07ca2b92b5b899031086a2792d6b62dc
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-01-19 16:59:47 +0900

    escape処理を追加

--- model/formModel.js
+++ model/formModel.js
@@ -1,9 +1,8 @@
 const connection = require('../database/database');
 
 module.exports = (name, title, body) => {
-    const query = 'INSERT INTO form_content (name, title, body) VALUES ("' + name + '", "' + title + '", "' + body + '")';
-    connection.query(query, (err, rows) => {
-        if (!err) console.log('ok');
-        console.log(err);
+    const query = 'insert into form_content set ?';
+    connection.query(query, { name: name, title: title, body: body }, (err, rows) => {
+        if (err) throw new Error('データの登録に失敗しました');
     });
 }
